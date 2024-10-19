f338d1bef6566487c6e17b3bbce393886ecedfb6
greenbeanoso <osogreenbean@gmail.com>
2024-10-19 16:12:50 +0800

    細分純視覺 + 算法解釋

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ else:
 
 # 將圖像進行縮放
 INimg = cv2.resize(INimg, (0, 0), fx=0.2, fy=0.2)
-def DoSubdivs(img,num):
-    w = img.shape[1]
-    h = img.shape[0]
-    Wsize = w / num
-    Hsize = h / num
+def DoSubdivs(Timg, num):
+    w = Timg.shape[1]  # 圖像寬度
+    h = Timg.shape[0]  # 圖像高度
+    Wsize = w // num  # 每個區塊的寬度
+    Hsize = h // num  # 每個區塊的高度
     
-def brighten(Aimg,Bimg):
+    for i in range(0, w, Wsize):
+        for j in range(0, h, Hsize):
+            if (i // Wsize + j // Hsize) % 2 == 0:
+                Timg[j:j+Hsize, i:i+Wsize] = [255, 255, 255]  # 白色區塊
+            else:
+                Timg[j:j+Hsize, i:i+Wsize] = [0, 0, 0]  # 黑色區塊
+
+    return Timg
+def brighten(Aimg,Bimg,value=1):
      # 將圖像轉為 int32，防止溢出
     Aimg = Aimg.astype(np.int32)
     Bimg = Bimg.astype(np.int32)
 
     # 所有通道一起加，然後進行範圍限制
-    Aimg += Bimg
+    Aimg += Bimg//value
     Aimg = np.clip(Aimg, 0, 255)
 
     # 最後轉回 uint8
@@ -45,24 +53,24 @@ def Update(img, br, bb, bg, cA, cB,subdivs):
     cv2.imshow('canny', edges)
     cv2.imshow('rgb+canny', brighten(img_copy, edges_colored))
     cv2.imshow('original+canny', brighten(img, edges_colored))
-    cv2.imshow('subdivs', DoSubdivs(img,subdivs))
+    cv2.imshow('subdivs', brighten(img, DoSubdivs(brighten(img_copy, edges_colored), subdivs),2))
 def TrackUpdate(val):
     br = cv2.getTrackbarPos('R', 'trackbar')
     bg = cv2.getTrackbarPos('G', 'trackbar')
     bb = cv2.getTrackbarPos('B', 'trackbar')
     cA = cv2.getTrackbarPos('CannyA', 'trackbar')
     cB = cv2.getTrackbarPos('CannyB', 'trackbar')
-    subdivs = cv2.getTrackbarPos('subdivs', 'trackbar')
+    subdivs = cv2.getTrackbarPos('subdivs', 'trackbar') +1
     Update(INimg, br, bb, bg,cA,cB,subdivs)
 # 創建一個視窗
-cv2.namedWindow('trackbar')
+cv2.namedWindow('trackbar', cv2.WINDOW_NORMAL)
 
 cv2.createTrackbar('R', 'trackbar', 0, 255, TrackUpdate)
 cv2.createTrackbar('G', 'trackbar', 0, 255, TrackUpdate)
 cv2.createTrackbar('B', 'trackbar', 0, 255, TrackUpdate)
 cv2.createTrackbar('CannyA', 'trackbar', 50, 500, TrackUpdate)
 cv2.createTrackbar('CannyB', 'trackbar', 50, 500, TrackUpdate)
-cv2.createTrackbar('subdivs', 'trackbar', 5, 10, TrackUpdate)
+cv2.createTrackbar('subdivs', 'trackbar', 5, 50, TrackUpdate)
 # 等待用戶按下 'r' 鍵來退出
 while True:
     key = cv2.waitKey(0) & 0xFF
